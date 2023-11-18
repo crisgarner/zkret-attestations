3b85c32ef3d7b4a3a636752917a0f4653e0bdca7
Crisgarner <@crisgarner>
2023-11-18 20:34:06 +0300

    verify proof

--- scripts/merkleroot.ts
+++ scripts/merkleroot.ts
@@ -1,45 +1,61 @@
-import {  Barretenberg, Fr } from '@aztec/bb.js';
+import { Fr } from '@aztec/bb.js';
 import { BarretenbergBackend } from '@noir-lang/backend_barretenberg';
 import { Noir } from '@noir-lang/noir_js';
+import { toHex } from 'viem'
 import { allowlist } from "./allowlist";
-import utils from "../circuits/utils/target/utils.json";
 import { MerkleTree } from './merkleTree';
+import circuit from "../circuits/target/attestation.json";
 
-//@ts-ignore
-let backendPedersen;
-//@ts-ignore
-let noirPedersen;
-let api:Barretenberg;
+
+let backend: BarretenbergBackend;
+let noir: Noir;
 
 
 async function initNoir() {
     //@ts-ignore
-    backendPedersen = new BarretenbergBackend(utils);
+    backend = new BarretenbergBackend(circuit);
     //@ts-ignore
-    noirPedersen = new Noir(utils, backendPedersen);
-    await noirPedersen.init();
-    api = await Barretenberg.new(/* num_threads */ 1);
-    api.pedersenInit();
+    noir = new Noir(circuit, backend);
+    await noir.init();
+    // api = await Barretenberg.new(/* num_threads */ 1);
+    // api.pedersenInit();
     console.log("Noir initiated");
 }
 
 async function createMerkleRoot() {
     const merkleTree = new MerkleTree(2);
     await merkleTree.initialize(allowlist.map((v) => Fr.fromString(v)));
-    console.log(merkleTree.root().toString());
-    console.log(merkleTree.getIndex(Fr.fromString(allowlist[0])));
-    console.log(await merkleTree.proof(0));
+    // console.log(merkleTree.root().toString());
+    // console.log(merkleTree.getIndex(Fr.fromString(allowlist[0])));
+    // console.log(await merkleTree.proof(0));
+    console.log("merkleTree generated.")
+    return merkleTree;
+}
+
+async function verify(merkleTree: MerkleTree, address: string) {
+    console.log('generating proof');
+    const index = merkleTree.getIndex(Fr.fromString(address));
+    const merkleproof = await merkleTree.proof(index);
+    const inputs = { leaf: merkleproof.leaf.toString() as `0x${string}`, index: index, hashpath: merkleproof.pathElements.map(x => x.toString()), root: merkleTree.root().toString() as `0x${string}`, };
+    //@ts-ignore
+    const proof = await noir.generateFinalProof(inputs);
+    console.log("Proof = ", toHex(proof.proof));
+    const r = await noir.verifyFinalProof(proof);
+    console.log("proof verified = ", r);
 }
 
 async function main() {
     await initNoir()
-    await createMerkleRoot();
+    const merkleTree = await createMerkleRoot();
+    await verify(merkleTree, "0xc59975735ed4774b3Ee8479D0b5A26388B929a34");
+    noir!.destroy();
+    backend!.destroy();
     //@ts-ignore
-    backendPedersen!.destroy();
+    // backendPedersen!.destroy();
     //@ts-ignore
-    noirPedersen!.destroy();
+    // noirPedersen!.destroy();
     //@ts-ignore
-    api!.destroy();
+    // api!.destroy();
 }
 
 main();

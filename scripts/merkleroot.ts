a6b366125a1cdb10ffc68e6258997bbb5fdb7a7e
Crisgarner <@crisgarner>
2023-11-18 15:01:34 +0300

    destroy

--- scripts/merkleroot.ts
+++ scripts/merkleroot.ts
@@ -20,24 +20,26 @@ async function initNoir() {
     console.log("Noir initiated");
 }
 
-function createMerkleRoot() {
-    const leafNodes = allowlist.map((addr: String) => keccak256(addr));
-    const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
+async function createMerkleRoot() {
+    const leafNodes = allowlist.map((addr: String) => pedersen(addr));
+    const merkleTree = new MerkleTree(leafNodes, pedersen, { sortPairs: true });
     const merkleRoot = merkleTree.getHexRoot();
     console.log('merkle root:', merkleRoot);
-    console.log('leaf 1', merkleTree.getHexProof(leafNodes[0]));
+    console.log('leaf 1', merkleTree.getHexProof(await leafNodes[0]));
 }
 
 async function pedersen(value: String) {
+    console.log(value);
     //@ts-ignore
     const result = await noirPedersen!.execute({ inputs: [value] });
-    console.log(result!.returnValue[0]);
+    return result!.returnValue[0];
 }
 
 async function main() {
     await initNoir()
-    await pedersen("0x417a472a0676a2d023431b1f052c78f38F44a800");
     createMerkleRoot();
+    backendPedersen!.destroy();
+    noirPedersen!.destroy();
 }
 
 main();

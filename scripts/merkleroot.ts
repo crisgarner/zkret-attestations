09c2c9e7383fe9fe6c337ce0cc9189f522913327
Oscar <o_rene89@protonmail.com>
2023-11-18 17:37:28 +0300

    Use different pedersen hash implementation

--- scripts/merkleroot.ts
+++ scripts/merkleroot.ts
@@ -1,9 +1,9 @@
-import { MerkleTree } from 'merkletreejs';
 import {  Barretenberg, Fr } from '@aztec/bb.js';
 import { BarretenbergBackend } from '@noir-lang/backend_barretenberg';
 import { Noir } from '@noir-lang/noir_js';
 import { allowlist } from "./allowlist";
 import utils from "../circuits/utils/target/utils.json";
+import { MerkleTree } from './merkleTree';
 
 //@ts-ignore
 let backendPedersen;
@@ -24,23 +24,11 @@ async function initNoir() {
 }
 
 async function createMerkleRoot() {
-    const leafNodes = await Promise.all(allowlist.map(async (addr: string) => pedersen(addr)));
-    const merkleTree = new MerkleTree(leafNodes, pedersen, { sortPairs: true });
-    const merkleRoot = merkleTree.getHexRoot();
-    console.log('merkle root:', merkleRoot);
-    // console.log('leaf 1', merkleTree.getHexProof(await leafNodes[0]));
-}
-
-async function pedersen(value: string|Buffer) {
-    const input = Fr.fromString(value.toString());
-    // const result = await api.pedersenCommit([input]);
-    const result =  await api.pedersenPlookupCommit([input])
-    console.log(`result is ${result}`);
-    return result.toBuffer();
-    //@ts-ignore
-    // const result = await noirPedersen!.execute({ inputs: [value] });
-    // console.log(`result is ${result!.returnValue[0]}`);
-    // return result!.returnValue[0];
+    const merkleTree = new MerkleTree(2);
+    await merkleTree.initialize(allowlist.map((v) => Fr.fromString(v)));
+    console.log(merkleTree.root().toString());
+    console.log(merkleTree.getIndex(Fr.fromString(allowlist[0])));
+    console.log(await merkleTree.proof(0));
 }
 
 async function main() {

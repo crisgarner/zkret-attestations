8c0c418a1433ca3323d9e3d9f7fbc069e13ef1a1
Oscar <o_rene89@protonmail.com>
2023-11-19 00:54:54 +0300

    Update workflow script

--- contracts/script/Workflow.s.sol
+++ contracts/script/Workflow.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {HelperConfig} from "./HelperConfig.sol";
 import {IEAS, AttestationRequest, AttestationRequestData} from "../src/IEAS.sol";
 import {ISchemaResolver} from "../src/resolver/ISchemaResolver.sol";
@@ -10,7 +10,7 @@ import {SchemaRegistry} from "../src/SchemaRegistry.sol";
 contract Workflow is Script {
     function run() public {
         HelperConfig helperConfig = new HelperConfig();
-        (address easAddr, address schemaRegistryAddr, address resolver, address zkVerifierAddress) =
+        (address easAddr, address schemaRegistryAddr, address attester, address zkVerifierAddress) =
             helperConfig.activeNetworkConfig();
         SchemaRegistry registry = SchemaRegistry(schemaRegistryAddr);
         IEAS eas = IEAS(easAddr);
@@ -21,9 +21,16 @@ contract Workflow is Script {
             abi.encode(bytes32(0x100a1699a37444f7f0809a13d20f4658f91299d514b954d6c91c3993df56e2ac));
 
         vm.startBroadcast();
-        bytes32 uuid = registry.register(schema, ISchemaResolver(resolver), false);
-        AttestationRequestData memory attestationData;
-        attestationData.data = merkleRoot;
+        bytes32 uuid =
+            registry.register(schema, ISchemaResolver(address(0xc944774D81AC1185C7BB5c3474777E117A1adE62)), false);
+        AttestationRequestData memory attestationData = AttestationRequestData({
+            recipient: address(0),
+            expirationTime: 0,
+            revocable: false,
+            refUID: bytes32(0),
+            data: merkleRoot,
+            value: 0
+        });
         AttestationRequest memory request = AttestationRequest({schema: uuid, data: attestationData});
         eas.attest(request);
         vm.stopBroadcast();
--- contracts/src/mocks/MockUltraVerifier.sol
+++ contracts/src/mocks/MockUltraVerifier.sol
@@ -2,9 +2,40 @@
 pragma solidity ^0.8.20;
 
 import {IUltraVerifier} from "../resolver/ZkResolver.sol";
+import {ISchemaResolver, Attestation} from "../resolver/ISchemaResolver.sol";
 
-contract MockUltraVerifier is IUltraVerifier {
+contract MockUltraVerifier is IUltraVerifier, ISchemaResolver {
     function verify(bytes calldata _proof, bytes32[] calldata _publicInputs) external view returns (bool) {
+        return true;
+    }
+
+    function isPayable() external pure override returns (bool) {
+        return false;
+    }
+
+    function attest(Attestation calldata attestation) external payable override returns (bool) {
+        return true;
+    }
+
+    function multiAttest(Attestation[] calldata attestations, uint256[] calldata values)
+        external
+        payable
+        override
+        returns (bool)
+    {
+        return true;
+    }
+
+    function revoke(Attestation calldata attestation) external payable override returns (bool) {
+        return true;
+    }
+
+    function multiRevoke(Attestation[] calldata attestations, uint256[] calldata values)
+        external
+        payable
+        override
+        returns (bool)
+    {
         return true;
     }
 }

93fa031732fb4c1b98a66d5ca5a845c043de7483
Crisgarner <@crisgarner>
2023-11-18 14:44:30 +0300

    main func

--- scripts/merkleroot.ts
+++ scripts/merkleroot.ts
@@ -1,8 +1,8 @@
-import { MerkleTree } from 'merkletreejs' ;
+import { MerkleTree } from 'merkletreejs';
 import keccak256 from 'keccak256';
-import {BarretenbergBackend} from '@noir-lang/backend_barretenberg';
-import {Noir} from '@noir-lang/noir_js';
-import {allowlist} from "./allowlist";
+import { BarretenbergBackend } from '@noir-lang/backend_barretenberg';
+import { Noir } from '@noir-lang/noir_js';
+import { allowlist } from "./allowlist";
 import utils from "../circuits/utils/target/utils.json";
 
 //@ts-ignore
@@ -11,7 +11,7 @@ let backendPedersen;
 let noirPedersen;
 
 
-async function initNoir(){
+async function initNoir() {
     //@ts-ignore
     backendPedersen = new BarretenbergBackend(utils);
     //@ts-ignore
@@ -21,19 +21,23 @@ async function initNoir(){
 }
 
 function createMerkleRoot() {
-    const leafNodes = allowlist.map((addr:String) => keccak256(addr));
+    const leafNodes = allowlist.map((addr: String) => keccak256(addr));
     const merkleTree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
     const merkleRoot = merkleTree.getHexRoot();
     console.log('merkle root:', merkleRoot);
     console.log('leaf 1', merkleTree.getHexProof(leafNodes[0]));
 }
 
-async function pedersen(value: String){
+async function pedersen(value: String) {
     //@ts-ignore
     const result = await noirPedersen!.execute({ inputs: [value] });
     console.log(result!.returnValue[0]);
 }
 
-initNoir()
-pedersen("0x417a472a0676a2d023431b1f052c78f38F44a800");
-createMerkleRoot();
+async function main() {
+    await initNoir()
+    await pedersen("0x417a472a0676a2d023431b1f052c78f38F44a800");
+    createMerkleRoot();
+}
+
+main();
